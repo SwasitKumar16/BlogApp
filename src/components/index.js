e3b7daa949b0c1011cf8a1a04ece3ee640393164
SwasitKumar16 <swasitkumardehury@gmail.com>
2024-11-26 22:50:38 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import RTE from "./RTE";
+import Select from "./Select";
+import Signup from "./Signup";
+import Login from "./Login"
+import Input from "./Input";
+import PostForm from "./post-form/PostForm.jsx"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
+
+export { Header, Footer, Container, Logo, LogoutBtn, Button, RTE, Select, Signup, Login, PostForm, PostCard, AuthLayout,Input };
